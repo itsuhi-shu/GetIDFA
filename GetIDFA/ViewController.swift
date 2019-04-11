f0352d6e576a91bc42d25f828eeaf6e56906315e
daikiZhou <zhouyifeig@gmail.com>
2019-04-11 11:00:34 +0900

    Initial Commit

---
+++ GetIDFA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetIDFA
+//
+//  Created by 周 軼飛 on 2019/04/11.
+//  Copyright © 2019 周 軼飛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
