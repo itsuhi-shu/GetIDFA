6c70ed20a701eed012244644415bc4bfc07902c5
itsuhi-shu <zhouyifeig@gmail.com>
2019-07-30 17:15:40 +0900

    first commit

--- GetIDFA/AppDelegate.swift
+++ GetIDFA/AppDelegate.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AdSupport
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    @objc dynamic var idfaStr: String!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -35,6 +37,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        idfaStr = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+        print(idfaStr ?? "")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- GetIDFA/ViewController.swift
+++ GetIDFA/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var label: UITextView!
+    var observation: NSKeyValueObservation?
+    @objc var appDelegate: AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label.isSelectable = true
+        label.isEditable = false
+        label.delegate = self
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textAlignment = .center
+        
+        view.addSubview(label)
+        
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        label.text = appDelegate.idfaStr
+        observation = observe(\.appDelegate.idfaStr, options: .new, changeHandler: { [weak self] (object, change) in
+            self?.label.text =  change.newValue!
+            
+            self?.label.selectAll(nil)
+        })
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        label.selectAll(nil)
+    }
 }
+
 
